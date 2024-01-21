f1bddcb871006a81ba7dba5dc8f01f08baddc9e9
Lynxnd <lynxndlovu@gmail.com>
2024-01-21 12:32:04 -0800

    Prints number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: exit success, exit failuer
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	if (argc >= 1)
+	{
+		printf("%d\n", argc - 1);
+		exit (EXIT_SUCCESS);
+	}
+	exit (EXIT_FAILURE);
+}
