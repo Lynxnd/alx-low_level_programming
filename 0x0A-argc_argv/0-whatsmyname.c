59ea87634ea903285081782e38ef6b57f432cea5
Lynxnd <lynxndlovu@gmail.com>
2024-01-21 12:13:10 -0800

    Add program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdlab.h>
+#include <stdio.h>
+
+/**
+ * main - print name of program
+ *
+ * @argc: argument count in the cmd line
+ *
+ * @argv: argument vector, arguments in the cmd line
+ *
+ * Return - Success
+ */
+
+int main(int argc, char* argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s", argv[0]);
+		exit (EXIT_SUCCESS);
+	}
+	exit (EXIT_FAILURE);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+Comand line arguments
+=> int main(int argc, char *argv[])
+	argc = total number of arguments in the comand line 
+	argv[] = Its a 2 dimensional point, point to the first argument which is always the name of the program.
+
+=> 
